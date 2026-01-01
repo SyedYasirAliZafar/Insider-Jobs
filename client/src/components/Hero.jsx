a8533410c7da9234c33aa83bad03bef213839668
SyedYasirAliZafar <yasirali1234zafar@gmail.com>
2026-01-02 03:27:01 +0500

    On Search Logic Done

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,10 +1,22 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef } from 'react'
 import { assets } from '../assets/assets'
 import { AppContext } from '../context/AppContext'
 
 function Hero() {
 
     const {setSearchFilter, setIsSearched} = useContext(AppContext)
+
+    const titleRef = useRef(null)
+    const locationRef = useRef(null)
+
+    const onSearch = () =>{
+        setSearchFilter({
+            title: titleRef.current.value,
+            location: locationRef.current.value
+        })
+        setIsSearched(true)
+        
+    }
 
   return (
     <div className='container 2xl:px-20 mx-auto my-10'>
@@ -14,13 +26,17 @@ function Hero() {
         <div className='flex items-center justify-between bg-white rounded text-gray-600 max-w-xl pl-4 mx-4 sm:mx-auto'>
             <div className='flex items-center '>
                 <img src={assets.search_icon} alt="" className='h-4 sm:h-5'/>
-                <input type="text" placeholder='Search for jobs' className='max-sm:text-xs p-2 rounded outline-none w-full'/>
+                <input type="text" placeholder='Search for jobs' className='max-sm:text-xs p-2 rounded outline-none w-full'
+                ref={titleRef}
+                />
             </div>
             <div className='flex items-center'>
                 <img src={assets.location_icon} alt="" className='h-4 sm:h-5' />
-                <input type="text" placeholder='Location' className='max-sm:text-xs p-2 rounded outline-none w-full'/>
+                <input type="text" placeholder='Location' className='max-sm:text-xs p-2 rounded outline-none w-full'
+                ref={locationRef}
+                />
             </div>
-            <button className='bg-blue-600 px-6 py-2 rounded text-white m-1 cursor-pointer active:scale-95 transition'>Search</button>
+            <button onClick={onSearch} className='bg-blue-600 px-6 py-2 rounded text-white m-1 cursor-pointer active:scale-95 transition'>Search</button>
         </div>
       </div>
 
