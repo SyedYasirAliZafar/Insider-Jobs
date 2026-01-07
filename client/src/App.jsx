4ddcb3529447401215992f36e83738f317d762db
SyedYasirAliZafar <yasirali1234zafar@gmail.com>
2026-01-08 01:56:31 +0500

    Recruiter Pages Routes Created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,10 @@ import ApplyJob from "./pages/ApplyJob"
 import Applications from "./pages/Applications"
 import RecruiterLogin from "./components/RecruiterLogin"
 import { AppContext } from "./context/AppContext"
+import Dashboard from "./pages/Dashboard"
+import AddJobs from "./pages/AddJobs"
+import ManageJobs from "./pages/ManageJobs"
+import ViewApplications from "./pages/ViewApplications"
 
 function App() {
 
@@ -20,6 +24,12 @@ function App() {
           <Route path="/" element={<Home/>} />
           <Route path="/apply-job/:id" element={<ApplyJob/>} />
           <Route path="/applications" element={<Applications/>} />
+
+          <Route path="/dashboard" element={<Dashboard/>}>
+            <Route path="add-job" element={<AddJobs/>}/>
+            <Route path="manage-jobs" element={<ManageJobs/>}/>
+            <Route path="view-applications" element={<ViewApplications/>}/>
+          </Route>
 
         </Routes>
       </div>
