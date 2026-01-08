f64f7b18ee6027305c18f239cb5eea023b94eddb
SyedYasirAliZafar <yasirali1234zafar@gmail.com>
2026-01-08 13:13:00 +0500

    View Application Page and Admin Dashboard Ui Completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Dashboard from "./pages/Dashboard"
 import AddJobs from "./pages/AddJobs"
 import ManageJobs from "./pages/ManageJobs"
 import ViewApplications from "./pages/ViewApplications"
+import 'quill/dist/quill.snow.css'
 
 function App() {
 
